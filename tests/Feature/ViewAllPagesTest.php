55f4ed225dcf2b2c7176026c673941ab8e62ea9e
amanda.fager@outlook.com <amanda.fager@outlook.com>
2021-04-13 19:05:51 +0200

    update test

--- tests/Feature/ViewAllPagesTest.php
+++ tests/Feature/ViewAllPagesTest.php
@@ -16,6 +16,30 @@ class ViewAllPagesTest extends TestCase
     public function test_view_index_page()
     {
         $response = $this->get('/');
-        $response->assertStatus(500);
+        $response->assertStatus(200);
+    }
+
+    public function test_view_posts_with_category_food_page()
+    {
+        $response = $this->get('/food');
+        $response->assertStatus(200);
+    }
+
+    public function test_view_posts_with_category_interior_page()
+    {
+        $response = $this->get('/interior');
+        $response->assertStatus(200);
+    }
+
+    public function test_view_posts_with_category_lifestyle_page()
+    {
+        $response = $this->get('/lifestyle');
+        $response->assertStatus(200);
+    }
+
+    public function test_view_posts_with_category_fashion_page()
+    {
+        $response = $this->get('/fashion');
+        $response->assertStatus(200);
     }
 }
